a18fbb648b9fbb0beffd51ff4bc413759b82a08d
lovricmarko <61913711+lovricmarko@users.noreply.github.com>
2020-12-06 17:37:21 +0100

    Frontend documents web arhitekture

---
+++ js/custom.js
@@ -0,0 +1,60 @@
+(function($) {
+    "use strict";
+	
+	/* ..............................................
+	   Učitavanje 
+	   ................................................. */
+	$(window).on('load', function() {
+		$('.preloader').fadeOut();
+		$('#preloader').delay(550).fadeOut('slow');
+		$('body').delay(450).css({
+			'overflow': 'visible'
+		});
+	});
+
+
+	/* ..............................................
+	   Galerija
+	   ................................................. */
+
+	$('#slides-shop').superslides({
+		inherit_width_from: '.cover-slides',
+		inherit_height_from: '.cover-slides',
+		play: 5000,
+		animation: 'fade',
+	});
+
+	$(".cover-slides ul li").append("<div class='overlay-background'></div>");
+
+	
+
+
+	/* ..............................................
+	   Scroll
+	   ................................................. */
+
+	$(document).ready(function() {
+		$(window).on('scroll', function() {
+			if ($(this).scrollTop() > 100) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		$('#back-to-top').click(function() {
+			$("html, body").animate({
+				scrollTop: 0
+			}, 600);
+			return false;
+		});
+	});
+	/* ..............................................
+	   NiceScroll
+	   ................................................. */
+
+	$(".brand-box").niceScroll({
+		cursorcolor: "#9b9b9c",
+	});
+	
+	
+}(jQuery));
